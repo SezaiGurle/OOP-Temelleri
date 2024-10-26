7403c3ecaa0dde21e81f060f2a6645e9d963a89d
Sezai Gürle <sezaigurle35@gmail.com>
2024-10-26 11:38:19 +0300

    added some new features

--- u01_oop_ilkUygulama/Personel.cs
+++ u01_oop_ilkUygulama/Personel.cs
@@ -1,31 +1,30 @@
 using System;
 
-namespace u01_oop_ilkUygulama
+namespace u01_oop_ilkUygulama;//personel sınıfı bu uzayda olsun
+
+public class Personel
 {
-    class Personel
+    //Fields(alanlar)
+    private int id;//dışaıdan erişilemez
+    private string ad;
+    private string soyad;
+
+    //Properties(Özellikler)
+    public int Id { get { return id; } }
+    public string Ad { get { return ad; } set { ad = value; } }
+    public string Soyad { get { return soyad; } set { soyad = value; } }
+
+    //Constructor (Yapıcı metotlar)
+    //Amaç: Personel oluşturulken id verilmesi
+    public Personel(int personelId)
     {
-        //Fields(alanlar)
-        private int id;
-        private string ad;
-        private string soyad;
-
-        //Properties(Özellikler)
-        public int Id { get { return id; } }
-        public string Ad { get { return ad; } set { ad = value; } }
-        public string Soyad { get { return soyad; } set { soyad = value; } }
-
-        //Constructor (Yapıcı Methodlar)
-        public Personel(int personelId)
-        {
-            id = personelId;
-        }
-
-        //Methods (Methodlar)
-        public void Yazdir()
-        {
-            Console.WriteLine($"{id}-{ad}-{soyad}");
-        }
+        Console.WriteLine("Personel yapici metodu çalisti.");
+        id = personelId;
     }
 
-
+    //Methods (metotlar)
+    public void Yazdir()
+    {
+        Console.WriteLine($"{id} - {ad} {soyad}");
+    }
 }
--- u01_oop_ilkUygulama/Program.cs
+++ u01_oop_ilkUygulama/Program.cs
@@ -1,12 +1,31 @@
-﻿using u01_oop_ilkUygulama;
+﻿﻿using u01_oop_ilkUygulama;//personel sınıfının uzayını kullan
 
-Personel p1 = new Personel(1);
+Personel p1 = new Personel(1);//bellekten p1 için yer ayır
 p1.Ad = "Hasan";
-p1.Soyad = "Demir";
+p1.Soyad = "DEMİR";
 
-Personel p2 = new Personel(2) {Ad = "Ayşe", Soyad="Can"};
+Personel p2 = new Personel(2) { Ad = "Ayşe", Soyad = "CAN"};
 
-Console.WriteLine("Personel 1'in bilgileri:");
+//p1.Id = 5; Id sadece okunabilir olduğu için değiştirilemez
+p1.Ad = "Mehmet";
+
+
+Console.WriteLine("Personel1'in Bilgileri");
 p1.Yazdir();
-Console.WriteLine("\n\nPersonel 2'nin bilgileri:");
+
+Console.WriteLine("\n\nPersonel2'in Bilgileri");
 p2.Yazdir();
+
+
+Mudur mudur = new Mudur(3);//idsi olmayan personel oluştu
+mudur.Ad = "Ali";
+mudur.Soyad = "Kara";
+
+mudur.CalisanEkle(p1);
+mudur.CalisanEkle(p2);
+
+mudur.CalisanlariListele();
+
+p1.Yazdir();
+p2.Yazdir();
+mudur.Yazdir();
