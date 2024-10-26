4e3a6e939aee8dcd3ec6bb7f15a4a3b6932edc4b
Sezai Gürle <sezaigurle35@gmail.com>
2024-10-26 15:34:51 +0300

    Added file things

--- u08_stack_queue/Program.cs
+++ u08_stack_queue/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Queue<int> kuyruk = new Queue<int>();
+
+kuyruk.Enqueue(3);
+kuyruk.Enqueue(8);
+kuyruk.Enqueue(2);
+
+Console.WriteLine("Queue:");
+while(kuyruk.Count>0)
+{
+    Console.WriteLine(kuyruk.Dequeue());
+}
+Console.WriteLine("Stack:");
+Stack<int> yigin = new Stack<int>();
+
+yigin.Push(3);
+yigin.Push(8);
+yigin.Push(2);
+
+while(yigin.Count>0)
+{
+    Console.WriteLine(yigin.Pop());
+}
---
+++ u09_dosyalama1/Program.cs
@@ -0,0 +1,11 @@
+﻿string yol1 = "C:\\Users";
+string yol2 = "Belgeler\\dosyalar.txt";
+
+string yol3= Path.Combine(yol1, yol2);//arasına iki tane \\ ekler 
+
+Console.WriteLine(yol3);
+
+Console.WriteLine("DirectoryName:" + Path.GetDirectoryName(yol3));
+Console.WriteLine("FileName:" + Path.GetFileName(yol3));
+Console.WriteLine("Extension:" + Path.GetExtension(yol3));
+
