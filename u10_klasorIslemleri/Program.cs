ccf80d3dc2c028cd3820fe3f32f5c12a38b961fb
Sezai Gürle <sezaigurle35@gmail.com>
2024-10-26 15:57:17 +0300

    C# dosyalama

--- u10_klasorIslemleri/Program.cs
+++ u10_klasorIslemleri/Program.cs
@@ -5,4 +5,14 @@ foreach(string klasor in klasorler)
     Console.WriteLine(klasor);
 }
 
-Directory.CreateDirectory("C:\\BTKAkademi");
+if(!Directory.Exists("C:\\BTKAkademi")){
+    Directory.CreateDirectory("C:\\BTKAkademi");
+}
+
+Directory.Delete("C:\\BTKAkademi");
+
+string masaustuYolu = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+Console.WriteLine("Masaüstü:" + masaustuYolu);
+
+Directory.CreateDirectory(Path.Combine(masaustuYolu,"BTK"));
---
+++ u11_dosyaIslemleri/Program.cs
@@ -0,0 +1,11 @@
+﻿string masaustuYolu = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+string btkYolu = Path.Combine(masaustuYolu,"BTK");
+string dosyaYolu = Path.Combine(btkYolu,"belge.txt");
+
+File.WriteAllText(dosyaYolu,"Merhaba Dünya!!");
+
+File.AppendAllText(dosyaYolu,"\nBu satır sonradan eklendi");
+
+string icerik = File.ReadAllText(dosyaYolu);
+
+Console.WriteLine("İçerik:"+icerik);
